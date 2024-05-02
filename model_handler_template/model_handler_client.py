04dbc4545e3eece0e9fac3ddfe2757a56a5ed9ec
bxbach732 <buixuanbach2000@gmail.com>
2024-05-02 15:59:07 +0300

    fix: fix startTask
    fix model choosing functionality according to guidance and add a simple client for testing

--- model_handler_template/model_handler_client.py
+++ model_handler_template/model_handler_client.py
@@ -7,11 +7,16 @@ import model_handler_pb2_grpc
 
 def run():
     print("calling Model Handler stub...")
-    # with grpc.insecure_channel("localhost:8061") as channel:
-    #     stub = model_handler_pb2_grpc.ModelHandlerStub(channel)
-    #     response = 
-    print("Model Handler client received: ")
-
+    with grpc.insecure_channel("localhost:8061") as channel:
+        stub = model_handler_pb2_grpc.ModelHandlerStub(channel)
+        #response = stub.startTask(model_handler_pb2.Response(response="Hello world"))
+        response = stub.startTask(model_handler_pb2.modelRequirements(
+                needs_text=True,
+                needs_image=False,
+                sessionID="test"
+            )
+        )
+        print("Model Handler client received: " + response)
 
 if __name__ == "__main__":
     logging.basicConfig()
--- model_handler_template/model_handler_server.py
+++ model_handler_template/model_handler_server.py
@@ -9,8 +9,31 @@ import model_handler_pb2_grpc
 
 port = 8061
 
-class ModelHandler(model_handler_pb2_grpc.CollaborateServicer):
+class ModelHandler(model_handler_pb2_grpc.ModelHandlerServicer):
   def __init__(self):
+    # self.model_list = [
+    #   {
+    #       'needs_text': True,
+    #       'needs_image': False,
+    #       'can_text': True,
+    #       'can_image': False,
+    #       'modelID': 'model_1'
+    #   },
+    #   {
+    #       'needs_text': False,
+    #       'needs_image': True,
+    #       'can_text': False,
+    #       'can_image': True,
+    #       'modelID': 'model_2'
+    #   },
+    #   {
+    #       'needs_text': True,
+    #       'needs_image': True,
+    #       'can_text': True,
+    #       'can_image': True,
+    #       'modelID': 'model_3'
+    #   }
+    # ]
     self.model_list = []
 
   def startTask(self, request, context):
@@ -18,28 +41,36 @@ class ModelHandler(model_handler_pb2_grpc.CollaborateServicer):
 
     #Scan in the model list for the suitable model
     for model in self.model_list:
-      if (request.need_text == model.can_text and request.need_image == model.can_image):
-        suitable_models_list.append(model)
+      if (request.needs_text and request.needs_image):
+        if (model["can_text"] and model["can_image"]):
+          suitable_models_list.append(model)
+      elif (request.needs_text):
+        if (model["can_text"] and not model["needs_image"]):
+          suitable_models_list.append(model)
+      elif (request.needs_image):
+        if (model["can_image"] and not model["needs_text"]):
+          suitable_models_list.append(model)
 
     #choose a random model if there are multiple that sastisfy the requirements
     chosen_model = random.choice(suitable_models_list)
+    print(chosen_model)
 
     #assign the modelID to the session
-    request.session["modelID"] = chosen_model.modelID
-
+    request.session["modelID"] = chosen_model["modelID"]
+    
     return model_handler_pb2.Empty()
 
   def finishTask(self, request, context):
-    taskMetrics = request.taskMetrics
-
+    taskMetrics = request
+    metrics = model_handler_pb2.metricsJson()
     return model_handler_pb2.metricsJson()
   
   def sendToModel(self, request, context):
-      
+    taskRequest = request
     return
   
   def returnToTask(self, request, context):
-      
+    modelAnwer = request
     return
   
   def registerModel(self, request, context):
