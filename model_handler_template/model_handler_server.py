afada5f2f220474785bcdcf568b1578f2e48e87c
bxbach732 <buixuanbach2000@gmail.com>
2024-05-03 06:12:08 +0300

    feat: add sendToModel and returnToTask
    all the services are done. Also modify the client side as a way to test. However, how it actually works with the task and model components is not known yet.

--- model_handler_template/model_handler_server.py
+++ model_handler_template/model_handler_server.py
@@ -12,54 +12,37 @@ port = 8061
 
 class ModelHandler(model_handler_pb2_grpc.ModelHandlerServicer):
   def __init__(self):
-    self.model_list = [
-      {
-          'needs_text': True,
-          'needs_image': False,
-          'can_text': True,
-          'can_image': False,
-          'modelID': 'model_1'
-      },
-      {
-          'needs_text': False,
-          'needs_image': True,
-          'can_text': False,
-          'can_image': True,
-          'modelID': 'model_2'
-      },
-      {
-          'needs_text': True,
-          'needs_image': True,
-          'can_text': True,
-          'can_image': True,
-          'modelID': 'model_3'
-      }
-    ]
-    #self.model_list = [] # a list of all the modelDefinition that got registered to the handler
+    self.model_list = [] # a list of all the modelDefinition that got registered to the handler
     self.assignment_list = {} # a dictionary for storing the modelID-sessionID connection
 
   def startTask(self, request, context):
-    modelRequirements = request
     suitable_models_list = []
+
+    modelRequirements = {
+        "needs_text": request.needs_text,
+        "needs_image": request.needs_image,
+        "sessionID": request.sessionID
+    }
 
     #Scan in the model list for the suitable model
     for model in self.model_list:
-      if (modelRequirements.needs_text and modelRequirements.needs_image):
+      if (modelRequirements["needs_text"] and modelRequirements["needs_image"]):
         if (model["can_text"] and model["can_image"]):
           suitable_models_list.append(model)
-      elif (modelRequirements.needs_text):
+      elif (modelRequirements["needs_text"]):
         if (model["can_text"] and not model["needs_image"]):
           suitable_models_list.append(model)
-      elif (modelRequirements.needs_image):
+      elif (modelRequirements["needs_image"]):
         if (model["can_image"] and not model["needs_text"]):
           suitable_models_list.append(model)
 
     #choose a random model if there are multiple that sastisfy the requirements
     chosen_model = random.choice(suitable_models_list)
+    print("The chosen model is")
     print(chosen_model)
 
     #connect the modelID to the sessionID
-    self.assignment_list[modelRequirements.sessionID] = chosen_model["modelID"]
+    self.assignment_list[modelRequirements["sessionID"]] = chosen_model["modelID"]
 
     return model_handler_pb2.Empty()
 
@@ -77,16 +60,34 @@ class ModelHandler(model_handler_pb2_grpc.ModelHandlerServicer):
   
   def sendToModel(self, request, context):
     taskRequest = request
-    return
+    modelID = self.assignment_list[taskRequest.sessionID]
+
+    return model_handler_pb2.modelRequest(
+      request = taskRequest.request,
+      modelID = modelID,
+      sessionID = taskRequest.sessionID
+    )
   
   def returnToTask(self, request, context):
     modelAnwer = request
-    return
+
+    return model_handler_pb2.modelAnswer(
+      answer = modelAnwer.answer,
+      sessionID = modelAnwer.sessionID
+    )
   
   def registerModel(self, request, context):
-    #add the models to the model list on startup
-    modelDefinition = request
-    self.model_list.append(modelDefinition)
+    print("===Register a model===")
+
+    modelDefinition = {
+        "needs_text": request.needs_text,
+        "needs_image": request.needs_image,
+        "can_text": request.can_text,
+        "can_image": request.can_image,
+        "modelID": request.modelID
+    }
+
+    self.model_list.append(modelDefinition) #add the models to the model list on startup
     return model_handler_pb2.Empty();
 
 async def serve():
