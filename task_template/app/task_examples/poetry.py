623a5c09c1ef31ce0443f7d4a86bcf60a776b902
bxbach732 <buixuanbach2000@gmail.com>
2024-06-12 15:06:01 +0300

    improve poetry prompt

--- task_template/app/task_examples/poetry.py
+++ task_template/app/task_examples/poetry.py
@@ -30,6 +30,7 @@ class Poetry(Task):
             [COMMENT_LINE] : the user comment on the poem up till now
             The comment line can be empty
             Your answer should take the comment line and the poem line into consideration and consist and consist of the next line in the poem you want to create.
+            Your anser should only be the poem line you created. Do not include the text '[POEM_LINE]' or '[COMMENT_LINE]' into your answer.
             """
         return system_prompt
 
