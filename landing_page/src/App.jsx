f919b6e040b7398754ecd422d4407d9d44622414
bxbach732 <buixuanbach2000@gmail.com>
2024-08-26 00:35:32 +0300

    feat: bootstrap landing page

---
+++ landing_page/src/App.jsx
@@ -0,0 +1,19 @@
+import Rules from "./components/TaskDescription";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./index.css";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Rules />
+      {/* <div className="card-list">
+        <CardList />
+      </div> */}
+      <Footer />
+    </>
+  );
+};
+
+export default App;
