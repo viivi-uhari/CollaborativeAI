c992fa0cafd8cdf1e353d8f29163509a9d350594
bxbach732 <buixuanbach2000@gmail.com>
2024-08-28 01:03:55 +0300

    feat: add animation and some styling to the card

--- landing_page/src/components/Card.jsx
+++ landing_page/src/components/Card.jsx
@@ -5,12 +5,18 @@ const Card = ({card}) => {
       <h2 className="card-name">{card.name}</h2>
       <p className="card-description">{card.description}</p>
       <div className="card-btn-group">
-        <a class="link-btn" type="button" href={card.task_link}>
-          <i class="fa fa-play"></i>        
-        </a>
-        <a class="code-btn" type="button" href={card.code_link}>
-          <i class="fa fa-code"></i>
-        </a>
+        <div className="task-btn-wrapper">
+          <a class="task-btn" type="button" href={card.task_link} style={{color: "rgb(176, 124, 124)"}}>
+            <i class="fa fa-play"></i>       
+          </a>
+          <p class="task-link-text">Launch task</p>
+        </div>
+        <div className="code-btn-wrapper">
+          <a class="code-btn" type="button" href={card.code_link} style={{color: "rgb(176, 124, 124)"}} >
+            <i class="fa fa-code"></i>
+          </a>
+          <p class="code-link-text">View code</p>
+        </div>
       </div>
     </div>
   );
