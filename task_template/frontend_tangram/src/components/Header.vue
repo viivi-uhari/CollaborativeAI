b5d83cdd290fd0d275464461758529c627bf2aaa
bxbach732 <buixuanbach2000@gmail.com>
2024-08-28 13:35:22 +0300

    feat: make tangram frontend consistent with landing page and the peotry page

---
+++ task_template/frontend_tangram/src/components/Header.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="header-container">
+    <div class="header-content">
+      <h1>🤖🤝🧑 Collaborative AI Arena</h1>
+      <h2 :style="{ fontWeight: 'normal' }">
+        Benchmarking collaborative capabilities of AI in the wild
+      </h2>
+    </div>
+    <nav class="navbar">
+      <a href="https://humane-ai.dice.aalto.fi/">Home</a>
+      <a href="#">Task</a>
+      <a href="#">About Us</a>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+.header-container {
+  background-color: rgb(76, 70, 163);
+  color: white;
+  display: flex;
+  justify-content: space-between; 
+  align-items: center; 
+  height: 150px; 
+  padding: 20px;
+}
+
+.navbar {
+  color: white;
+  display: flex;
+}
+
+.navbar a {
+  padding: 5px;
+}
+
+a:link {
+  color: white;
+  background-color: transparent;
+  text-decoration: none;
+}
+
+a:visited {
+  color: white;
+  background-color: transparent;
+  text-decoration: none;
+}
+
+a:hover {
+  color: red;
+  background-color: transparent;
+  text-decoration: underline;
+}
+
+a:active {
+  color: yellow;
+  background-color: transparent;
+  text-decoration: underline;
+}
+</style>
