8ae5237c10a69ac0370d40dbec7e587f6a4b1cdc
Thomas Pfau <github@thomaspfau.de>
2024-09-19 11:32:28 +0300

    Removing SSL from uvicorn, now handled in ingress

--- task_template/entrypoint.sh
+++ task_template/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-echo "$SSL_CERTIFICATE" >> cert.pem
-echo "$SSL_KEY" >> key.pem
-uvicorn main:app --ssl-keyfile /usr/project/key.pem --ssl-certfile /usr/project/cert.pem --port 8062 --host 0.0.0.0
+# For HTTPS use uncomment the following 3 lines
+# echo "$SSL_CERTIFICATE" >> cert.pem
+# echo "$SSL_KEY" >> key.pem
+# uvicorn main:app --ssl-keyfile /usr/project/key.pem --ssl-certfile /usr/project/cert.pem --port 8062 --host 0.0.0.0
+
+# if you use https, comment this line
+uvicorn main:app --port 8062 --host 0.0.0.0
