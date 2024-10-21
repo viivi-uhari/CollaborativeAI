c301199a57e089b7f161aee9c6b1a3152beecabc
Badó Miklós Máté <68738628+badomate@users.noreply.github.com>
2024-10-21 09:49:23 +0200

    Update task_template/entrypoint.sh
    Co-authored-by: Thomas Pfau <github@thomaspfau.de>

--- task_template/entrypoint.sh
+++ task_template/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "$SSL_CERTIFICATE" >> cert.pem
 echo "$SSL_KEY" >> key.pem
-uvicorn main:app --ssl-keyfile /usr/project/key.pem --ssl-certfile /usr/project/cert.pem --port 8062 --host 0.0.0.0
+uvicorn main:app --port 8062 --host 0.0.0.0
