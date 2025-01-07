6194b8248c77fa81d9f9fd782f382d4b6d6be0bd
Thomas Pfau <github@thomaspfau.de>
2025-01-07 08:31:23 +0200

    Fixing orchestrator crash when Metrics are not selected

--- task_template/app/routers/task_router.py
+++ task_template/app/routers/task_router.py
@@ -5,13 +5,12 @@ from models import (
     ModelResponse,
     TaskRequirements,
     TaskMetrics,
-    OpenAIBasedDataRequest
+    OpenAIBasedDataRequest,
 )
 from routers.router_models import (
     ConversationItem,
     TaskRequest,
     SessionData,
-    
 )
 from routers.session import get_session, clear_session
 from typing import Dict, List, Annotated
@@ -27,16 +26,17 @@ logger = logging.getLogger(__name__)
 task_router = APIRouter(prefix="/api/v1/task")
 
 from uuid import uuid4
+import json
 
 
 @task_router.post("/completions")
 async def chat_completion_endpoint(
-    task_data: OpenAIBasedDataRequest, 
+    task_data: OpenAIBasedDataRequest,
     task_handler: Annotated[CompletionService, Depends(CompletionService)],
-    session: SessionData = Depends(get_session)
+    session: SessionData = Depends(get_session),
 ) -> TaskDataResponse:
     """
-    More "openAI" like endpoint, which takes a set of messages along with additional input data. 
+    More "openAI" like endpoint, which takes a set of messages along with additional input data.
     NOTE: the Messages are not allowed to contain a SYSTEM message, as the system message has to be added
     during task processing IN the server as to not allow free use of an endpoint for general chatting!
     """
@@ -58,7 +58,7 @@ async def chat_completion_endpoint(
     # Submit the task to the model
     logger.info("Task started, submitting to model")
 
-    # get the messages that have a role of system from the task_data    
+    # get the messages that have a role of system from the task_data
     model_request = task_handler.build_model_request_from_open_AI_request(task_data)
     # Add the session ID to the model request
     model_request.sessionID = sessionID
@@ -79,9 +79,9 @@ async def chat_completion_endpoint(
 
 @task_router.post("/process")
 async def process_task_data(
-    task_data: TaskDataRequest, 
+    task_data: TaskDataRequest,
     task_handler: Annotated[CompletionService, Depends(CompletionService)],
-    session: SessionData = Depends(get_session)
+    session: SessionData = Depends(get_session),
 ) -> TaskDataResponse:
     """Generate prompt endpoint:
     process pieces' data and plug them into the prompt
@@ -131,11 +131,12 @@ async def finish(
     delete the session based on the session_id cookie when the user decides
     their task is done
     """
-    finishObj = {"sessionID": session.id, "metrics": str(request.metrics)}
+    finishObj = {"sessionID": session.id, "metrics": json.dumps(request.metrics)}
+    logger.info(finishObj)
     queue_handler.finish_queue.put(finishObj)
     while not session.id in queue_handler.model_info:
         await asyncio.sleep(1)
-    modelinfo = queue_handler.get_model_info(session.id)    
+    modelinfo = queue_handler.get_model_info(session.id)
     # Clear the model info
     queue_handler.clear_session(session.id)
     clear_session(source_request)
